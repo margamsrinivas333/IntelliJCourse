d956a4c1a965065a759bd49247dda10d41d5398e
Srinivas <margamsrinivas333@gmail.com>
2024-07-09 15:56:27 -0400

    1st commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,34 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
+
+/*
+class H1
+{
+    public void method1()
+    {
+        System.out.println("Test1");
+    }
+
+    public void method2(int a, int b)
+    {
+        int i = 0;
+        for (int i1 = 0; i1 < i; i1++) {
+            System.out.println("for");
+        }
+
+        System.out.println("Test2");
+    }
+
+    public void method3(int b)
+    {
+        System.out.println("Test3");
+        RuntimeException runtimeException = new RuntimeException();
+    }
+}
+*/
+
---
+++ src/main/java/TestSignatureTest.java
@@ -0,0 +1,7 @@
+/*
+public class TestSignatureTest<k> {
+    public static void main(String[] args) {
+        TestSignatureTest<integer> TestSignatureTest = new TestSignatureTest<integer>();
+    }
+}
+*/
